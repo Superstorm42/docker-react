d2955b3ef54658422e72aa2e0729adaa8686cb28
Superstorm42 <riasatali4242@gmail.com>
2021-01-05 19:53:17 +0600

    push to feature repo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With AKash
+          Learn React With AKash, feature branch.
         </a>
       </header>
     </div>
